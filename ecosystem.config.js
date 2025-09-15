b4c4b17537029b35f71bbe35d84bfa1dd9404157
Doragon Foxie <tan.long.9699@gmail.com>
2025-09-15 22:16:46 +0700

    add ecosystem pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	apps : [{
+		name: "my-nest-app",
+		script: "./dist/main.js"
+		instances: "max",
+		exec_mode: "cluster",
+		env_production: {
+			NODE_ENV: "production",
+		}
+	]},
+}
