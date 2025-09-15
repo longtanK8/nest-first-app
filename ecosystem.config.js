00c898e8600c6e046c1ad44e2977d213294796a1
Doragon Foxie <tan.long.9699@gmail.com>
2025-09-15 23:10:34 +0700

    harcode instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps : [{
 		name: "my-nest-app",
 		script: "./dist/main.js",
-		instances: "max",
+		instances: "4",
 		exec_mode: "cluster",
 		env_production: {
 			NODE_ENV: "production",
