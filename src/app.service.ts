97ad4023ec43bc864a03fb6bbc713341d0ddebf6
Doragon Foxie <tan.long.9699@gmail.com>
2025-09-28 21:28:56 +0700

    change main text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'test pipeline = ))';
+    return 'em Hiep giau node cua toi dau roi';
   }
 }
